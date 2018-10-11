63ff1c8e2ddc73a303c0d50afd17ca698bc54e57
Anna Tataranovich <steshan86@gmail.com>
2018-10-11 09:34:33 +0300

    Remove register and forgot/reset password functionality.

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,6 @@ Route::get('/data/names', 'DataController@getNames');
 Route::get('/data/forms', 'DataController@getForms');
 Route::get('/data/component', 'DataController@getComponent');
 
-Auth::routes();
-
-/*
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-*/
-
-Route::get('/home', 'HomeController@index')->name('home');
